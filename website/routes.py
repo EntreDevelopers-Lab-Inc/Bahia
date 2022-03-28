da01621994f5a2b7eaa27886701e51d884a872fa
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2022-03-27 23:55:11 -0400

    preliminary nft data display

--- website/routes.py
+++ website/routes.py
@@ -11,9 +11,9 @@ def home():
 
 @app.route('/buy')
 def buy():
-    return render_template('sell.html')
+    return render_template('sell.html', mustache=True)
 
 
 @app.route('/sell')
 def sell():
-    return render_template('sell.html')
+    return render_template('sell.html', mustache=True)
--- website/static/assets/js/sell/sell.js
+++ website/static/assets/js/sell/sell.js
@@ -1,15 +1,95 @@
 // load moralis
 Moralis.start({serverUrl: MORALIS_SERVER_URL, appId: MORALIS_APP_ID});
 
-function loadNFTs()
+// load the nft template and objects
+const NFT_TEMPLATE = $('#nft-template').html();
+var NFT_OBJECTS = $('$nft-objects');
+
+// keep the sale data on hand
+var saleData = {};
+
+
+// synchronous function for adding an nft (will want to do this sequentially to maintain structure for the user)
+function addNFT(data)
 {
-    Moralis.Web3API.account.getNFTs({address: window.ethereum.selectedAddress, chain: CHAIN_ID_STR}).then(function(resp) {console.log(resp)});
+    // render the template
+    var newNft = Mustache.render(NFT_TEMPLATE, data);
+
+    // add the nft to the objects
+    NFT_OBJECTS.append(newNft);
 }
 
 
-// load document function
-function loadDocument()
+// load the nfts
+async function loadNFTs()
 {
+    // get all the nfts from the moralis api
+    Moralis.Web3API.account.getNFTs({address: window.ethereum.selectedAddress, chain: CHAIN_ID_STR}).then(function(resp) {
+        // add each nft from teh result
+        var nfts = resp.result;
+
+        // iterate over the nfts and add them to the list
+        for (var i = 0; i < nfts.length; i += 1)
+        {
+            addNFT(nfts[i]);
+        }
+    });
+}
+
+
+// load the past sales
+async function loadSales()
+{
+    // get the length of the number of sales
+
+    // fill the list backwards with the relevant information
+}
+
+
+// select an nft (cannot be asynchronous because other data depends on it)
+function selectNFT(address, id)
+{
+    // deselect the other selections
+
+    // select this tab as active
+
+    // write the sale dict
+    saleDict['address'] = address;
+    saleDict['id'] = id;
+}
+
+
+// create a sale
+async function createSale()
+{
+    // get all the information necessary for the sale from the frontend
+
+    // create a transaction
+        // get the number of transactions
+        // add the new sale the transaction log at the bottom of the page
+
+}
+
+// load document function
+async function loadDocument()
+{
+    // set the date attribute
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if (dd < 10) {
+       dd = '0' + dd;
+    }
+
+    if (mm < 10) {
+       mm = '0' + mm;
+    }
+
+    today = yyyy + '-' + mm + '-' + dd;
+    document.getElementById("date").setAttribute("min", today);
+
     loadNFTs();
 }
 

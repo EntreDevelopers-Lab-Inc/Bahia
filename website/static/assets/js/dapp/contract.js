e8bc0ccc66e5f76fea3ca0418566f604735ee90a
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2022-04-14 11:32:45 -0400

    whitepaper static text

--- website/static/assets/js/dapp/contract.js
+++ website/static/assets/js/dapp/contract.js
@@ -1,3 +1,10 @@
+// check if there is a provider
+if (window.ethereum == undefined)
+{
+    alert('Unable to connect to Web3 provider. Rerouting to home page...');
+    location.href = '/';
+}
+
 // contract provider https://github.com/mikec3/my_tutorials/blob/master/simple_storage/src/SimpleStorage.js
 let CONTRACT_PROVIDER = new ethers.providers.Web3Provider(window.ethereum, network=CHAIN_STRING);
 

93cd9f42b440af9764e604d60317f4fb2bff81cb
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2022-07-05 21:20:55 -0400

    made start page

--- website/__init__.py
+++ website/__init__.py
@@ -8,8 +8,10 @@ app = Flask(__name__)
 # run it!
 CORS(app, supports_credentials=True)
 from website.routes.barter import barter
+from website.routes.pool import pool
 
 app.register_blueprint(barter, url_prefix='/barter')
+app.register_blueprint(pool, url_prefix='/pool')
 
 if __name__ == '__main__':
     app.run(threaded=True)

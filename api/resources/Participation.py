9eee5c6089b9ac3fd67455c74c07fa52023ce2a9
naveenailawadi <naveen.ailawadi91@gmail.com>
2022-07-15 02:37:09 +0000

    authing the webhook

--- api/resources/Participation.py
+++ api/resources/Participation.py
@@ -1,3 +1,4 @@
+from api.site_secrets import RINKEBY_WEBHOOK_AUTH
 from api.models import db, ParticipationModel
 from api.resources import load_json
 from api.tools.pool import POOL_CONTRACT
@@ -11,12 +12,19 @@ class BaseParticipationResource(Resource):
     def post(self):
         data = load_json()
 
+        # print the data (until it is live)
+        print(data)
+
         # get the function input
         function, input_data = POOL_CONTRACT.decode_function_input(
             data['input'])
 
+        # if it is not our webhook, deny it
+        if (input_data.get('apiKey') != RINKEBY_WEBHOOK_AUTH):
+            return {'message': 'write access not permitted'}, 401
+
         # if the function name is not joinging the pool, just return
-        if function.fn_name != 'joinPool':
+        if (function.fn_name != 'joinPool') or (input_data['status'] != 'confirmed'):
             return {'status': 'no computation necessary'}, 202
 
         # if the participation exists, exit

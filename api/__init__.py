07cd79b5d219363730f4f0c0e2f97f87ebc63f48
naveenailawadi <naveen.ailawadi91@gmail.com>
2022-07-15 01:49:56 +0000

    enabled db

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,4 @@
-#from site_secrets import SECRET_KEY, SQLALCHEMY_DATABASE_URI
+from api.site_secrets import SECRET_KEY, SQLALCHEMY_DATABASE_URI
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -7,10 +7,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
-'''app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-'''
+
 # need to enable with this: https://askubuntu.com/questions/1222935/apache-flask-permission-denied-writing-to-var-www-even-though-the-folders-are
 
 db = SQLAlchemy(app)

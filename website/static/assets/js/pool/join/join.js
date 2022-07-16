5d5c678a2adf69cc12ae1e87eebd63f2d8e47798
Daniel Yamagata <danielyamagata@Daniels-MBP-7.hsd1.wa.comcast.net>
2022-07-16 13:30:17 -0700

    Preliminary work

---
+++ website/static/assets/js/pool/join/join.js
@@ -0,0 +1,35 @@
+// // Get modal elment 
+// var joinModal = document.getElementById('join-modal')
+
+// //Get open modal button
+// var joinBtn = document.getElementById('join-btn');
+
+// // Get close button
+// // var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+// // Listen for open click
+// joinBtn.addEventListener('click', openModal)
+
+// // // Listen for close click 
+// // closeBtn.addEventListener('click', closeModal);
+
+// // Listen for outside click
+// window.addEventListener('click', clickOutside)
+
+
+// function openModal(){
+//     joinModal.style.visibility = 'visible';
+//     joinModal.style.opacity = 1;
+//     console.log("Is this working?")
+
+// }
+
+// // function closeModal() {
+// //     modal.style.display = 'none';
+// // }
+
+// function clickOutside(e) {
+//     if(e.target == modal) {
+//     joinModal.style.visiblity = 'hidden';
+//     }
+// }
